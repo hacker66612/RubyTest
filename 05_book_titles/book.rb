cc2e26f15757bfe8316389c55bbf910a695b1223
hacker66612 <nikitabelikh@gmail.com>
2017-08-18 14:34:44 +0500

    ац

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
-end
+
+def titleize word 
+	 wordArray = word.split(" ")
+	 littleWords = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or",  "nor", "over"]
+	 wordArray.each_index do |x|
+		 if not (littleWords.include?(wordArray[x])) or x==0 then
+		 wordArray[x].capitalize!
+		 end
+		 end
+		 wordArray.join(" ")
+		 	 
+	end
+
+def title
+	@title
+	end
+
+def title= title 
+	@title = titleize(title)
+	end
+	
+
+	end
